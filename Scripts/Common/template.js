6eb9173096d51a11dac91a2411da0ab35e51bd7e
fangqj <fangqj@Ctrip.com>
2016-08-05 13:59:36 +0800

    =edit template get

--- Scripts/Common/template.js
+++ Scripts/Common/template.js
@@ -1,46 +1,54 @@
 ﻿define(['underscore'], function (_) {
-    var template = {
+    var templateC = {
         cache: {},
-        dataTepml: { dom: null, data: null, url: null, uniqueID: null, callback: null },
+        dataTepml: { dom: null, datas: null, url: null, uniqueID: null, callback: null },
         load: function (data) {
             if (data && data.url && data.url.length > 0) {
-                var content = template.load.getCache(data.url);
+                var content = templateC.getCache(data.url);
                 if (content) {
-                    template.load.render(data, content);
+                    templateC.render(data, content);
                 }
                 else {
-                    template.load.loadTemplate(data, content);
+                    templateC.loadTemplate(data, content);
                 }
             }
-        } .method("loadTemplate", function (data) {
+        },
+        loadTemplate: function (data) {
             var xhr = $.ajaxSettings.xhr();
             xhr.open("GET", data.url, true);
             xhr.send();
             xhr.onreadystatechange = function () {
                 if (xhr.readyState == 4) {
-                    template.setCache(data.url, xhr.responseText);
-                    template.load.render(data, xhr.responseText);
+                    templateC.setCache(data.url, xhr.responseText);
+                    templateC.render(data, xhr.responseText);
                 }
             }
-        }).method("setCache", function (key, value) {
-            template.cache = template.cache || {};
-            template.cache[key] = value;
-        }).method("getCache", function (key) {
-            template.cache = template.cache || {};
-            return template.cache[key];
-        }).method("render", function (data, templateContent) {
+        },
+        setCache: function (key, value) {
+            templateC.cache = templateC.cache || {};
+            templateC.cache[key] = value;
+        },
+        getCache: function (key) {
+            templateC.cache = templateC.cache || {};
+            return templateC.cache[key];
+        },
+        render: function (data, templateContent) {
             if (data && data.uniqueID && data.uniqueID.length > 0 && templateContent && templateContent.length > 0) {
-                var indexStart = templateContent.indexOf("<" + uniqueID + ">");
-                indexStart += uniqueID.length + 2;
-                var indexEnd = templateContent.indexOf("</" + uniqueID + ">");
-                if (indexStart > 0 && indexEnd > 0 && indexStart > indexEnd) {
-                    var templ = templateContent.substring(indexStart, indexEnd);
-                    data.dom && data.dom.html(_.template(templ, data.data));
+                var indexStart = templateContent.indexOf("<" + data.uniqueID + ">");
+                indexStart += data.uniqueID.length + 2;
+                var indexEnd = templateContent.indexOf("</" + data.uniqueID + ">");
+                if (indexStart > 0 && indexEnd > 0 && indexStart < indexEnd) {
+                    var templHtml = templateContent.substring(indexStart, indexEnd);
+                    debugger;
+                    var templ = _.template(templHtml);
+                    var html = templ(data.datas);
+                    data.dom && data.dom.html(html);
                 }
             }
             data.callback && data.callback();
-        })
+        }
 
-    }
-    return template;
+    };
+
+    return templateC;
 });
