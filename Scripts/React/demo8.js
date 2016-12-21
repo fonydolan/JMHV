b5cff2811b7bb9ac39b5b109aa3ab0c8c12a1fac
fangqj <fangqj@Ctrip.com>
2016-12-21 20:58:05 +0800

    =edit

--- Scripts/React/demo8.js
+++ Scripts/React/demo8.js
@@ -14,23 +14,36 @@ ReactDOM.render(
 */
 
 var LikeButton = React.createClass({
+    //组件中只执行一次 设置属性props 相当于静态方法
+    getDefaultProps:function(){
+        return {
+            testP:"fwe",
+            id:0
+        }
+    },
+    //每个实例中执行一次 设置状态变量state 相当于构造函数
     getInitialState:function(){
         return {liked:false};
     },
     handleClick:function(){
         this.setState({liked:!this.state.liked});
     },
+    propTypes:{
+        id:React.PropTypes.number.isRequired,
+        testP:React.PropTypes.string.isRequired,
+        onClick:React.PropTypes.func
+    },
     render:function () {
         var text = this.state.liked?'like':'haven\'t liked';
         return (
-            <p onClick={this.handleClick}>
-                You {text} this. Click to toggle.
+            <p key={this.props.key} onClick={this.handleClick}>
+                You {this.props.testP} {this.props.id} {text} this. Click to toggle.
             </p>
         );
     }
 });
-
+var uniqueID = "tID";
 ReactDOM.render(
-    <LikeButton />,
+    <LikeButton key={uniqueID}/>,
     document.getElementById('index')
 )
